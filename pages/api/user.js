23935934f96f907ec32ffd90d6462c2644488869
fanweixiao <fanweixiao@gmail.com>
2021-10-13 22:53:59 +0800

    change http errcode. TODO: do not query DB if prefix is visitor-

--- pages/api/user.js
+++ pages/api/user.js
@@ -12,7 +12,7 @@ const user = async (req, res) => {
             if (result.length) {
                 res.status(200).json({ data: result[0], msg: 'success' })
             } else{
-                res.status(404).json({ data: null, msg: 'success' })
+                res.status(200).json({ data: null, msg: 'success' })
             }
         } else {
             res.status(400).json({ msg: 'Missing parameter: "login" is required.' })
