e1a6a70e2e0af1aa58ba8fffa72a6169014604f8
fanweixiao <fanweixiao@gmail.com>
2021-10-31 21:33:29 +0800

    mapping country to mesh region

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,13 +2,43 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
   const { nextUrl: url, geo } = req
-  const country = geo.country || 'CN'
-  const city = geo.city || 'Beijing'
-  const region = geo.region || 'AS'
+  const country = geo.country
+  const city = geo.city
+  const mesh = getMeshID(country);
 
   url.searchParams.set('country', country)
   url.searchParams.set('city', city)
-  url.searchParams.set('region', region)
+  url.searchParams.set('region', mesh)
 
   return NextResponse.rewrite(url)
+}
+
+function getMeshID(country) {
+  if (country == undefined) {
+    return "us.x.yomo.dev";
+  }
+
+  if (country === "CN") {
+    return "can.x.yomo.dev";
+  }
+
+  const res = countryRegion.find((item) => {
+    if (item.name === country) {
+      return item.region;
+    }
+  });
+
+  if (!res) {
+    return "us.x.yomo.dev";
+  }
+
+  console.log("->res", res);
+  switch (res.region) {
+    case "Asia":
+      return "sg.x.yomo.dev";
+    case "Europe":
+      return "de.x.yomo.dev";
+    default:
+      return "us.x.yomo.dev";
+  }
 }
