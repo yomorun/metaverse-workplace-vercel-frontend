dbff2953deef29fe67ea7956c1ce390c057c20e7
Hong Xiaojian <xiaojian.hong@gmail.com>
2022-03-23 15:13:40 +0800

    chore: clean code

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,37 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { v1 } from "@datadog/datadog-api-client";
-import { env } from 'process';
+import { v1 } from '@datadog/datadog-api-client'
 
-const configuration = v1.createConfiguration();
-const apiInstance = new v1.MetricsApi(configuration);
+const configuration = v1.createConfiguration()
+const apiInstance = new v1.MetricsApi(configuration)
 
 const metrics = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-      const { userCountry, meshID, timestamp, latency } = req.body
-      const params: v1.MetricsApiSubmitMetricsRequest = {
-        body: {
-          series: [
-            {
-              metric: process.env.DD_LATENCY_METRICS_NAME || 'yomo.vhq.latency',
-              type: 'gauge',
-              points: [[timestamp / 1000, latency]],
-              tags: [`country:${userCountry}`, `mesh:${meshID}`],
+    if (req.method === 'POST') {
+        const { userCountry, meshID, timestamp, latency } = req.body
+        const params: v1.MetricsApiSubmitMetricsRequest = {
+            body: {
+                series: [
+                    {
+                        metric: process.env.DD_LATENCY_METRICS_NAME || 'yomo.vhq.latency',
+                        type: 'gauge',
+                        points: [[timestamp / 1000, latency]],
+                        tags: [`country:${userCountry}`, `mesh:${meshID}`],
+                    },
+                ],
             },
-          ],
-        },
-      };
-    
+        }
 
-      try {
-      const data: v1.IntakePayloadAccepted = await apiInstance
-        .submitMetrics(params);
-        res.status(200).json(data)
-      } catch(error: any) {
-        res.status(500).json({ err: error})
-      };
-  } else {
-      res.status(405).json({})
-  }
+        try {
+            const data: v1.IntakePayloadAccepted = await apiInstance.submitMetrics(params)
+            res.status(200).json(data)
+        } catch (error: any) {
+            res.status(500).json({ err: error })
+        }
+    } else {
+        res.status(405).json({})
+    }
 }
 
 export default metrics
