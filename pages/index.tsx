178d51774bef39a6e4231e0cf3563afcee584dab
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2022-08-09 12:46:51 +0800

    feat: show cookies consent base on cookie value (#44)
    * feat: show using cookies tips base on cookie value
    
    * fix: quit debug mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import { useEffect } from 'react'
+import CookieConsent from 'react-cookie-consent'
 
 import Sidebar from '../components/minor/sidebar'
 import FloorLinks from '../components/minor/floor-links'
@@ -38,6 +39,26 @@ const Home: Page<Location> = ({ country, region }) => {
                 </title>
             </Head>
             <div className='w-screen h-screen flex justify-center items-center bg-color-newyear'>
+                <CookieConsent
+                    location='top'
+                    buttonText='Accept'
+                    style={{
+                        width: '50%',
+                        marginLeft: '25%',
+                        marginTop: '5px',
+                        backgroundColor: 'rgba(51,59,97,0.8)',
+                        fontSize: '18px',
+                        border: '4px solid #333B61',
+                        color: 'rgb(238,207,74)',
+                    }}
+                    buttonStyle={{
+                        backgroundColor: '#5AB18F',
+                        color: '#303756',
+                        fontWeight: 700,
+                    }}
+                >
+                    This website uses cookies to enhance the user experience.
+                </CookieConsent>
                 <Sidebar />
                 <Scene
                     className='w-1800px min-w-1800px h-900px'
